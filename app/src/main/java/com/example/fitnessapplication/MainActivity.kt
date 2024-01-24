85d20125a2c322ca9cca6b5d64903d8b0b391105
Steponas <steponas2003@gmail.com>
2024-01-24 16:54:20 +0200

    added second activity empty

---
+++ app/src/main/java/com/example/fitnessapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.fitnessapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.button)
+        startButton.setOnClickListener {
+            val intent = Intent(this,WorkoutActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fitnessapplication/WorkoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fitnessapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class WorkoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_workout)
+    }
+}
